ceadbe6336900bc75aab53c1f38ea44128158891
Sudhanshu <bsudhanshu3@gmail.com>
2023-04-19 20:10:19 +0530

    header and footer UI implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'lightMetrics-assign';
+export class AppComponent implements OnInit, AfterViewInit {
+  ngAfterViewInit(): void {
+    this.adjustSearchPosition();
+  }
+
+  ngOnInit(): void {}
+
+  adjustSearchPosition() {
+    const headerSearch = document.getElementById('headerSearch')!;
+    const searchHeight = getComputedStyle(headerSearch).height;
+    headerSearch.style.marginBottom = `-${parseInt(searchHeight) / 2}px`;
+  }
 }
